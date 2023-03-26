a1ff398871c8e0f5947ee57864ab62e65f0a58d5
Mohammad Hassan shaikh <thehasanshaikh@gmail.com>
2023-03-26 23:54:51 +0530

    added home component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Login from './pages/login/Login';
 import BookRegistrationPage from './pages/bookRegistrationPage/BookRegistrationPage';
 import StartingSearchPage from './pages/startingSearchPage/StartingSearchPage'
 import Footer from './Component/Footer/Footer'
+import { Home } from './pages/Home/Home';
 
 function App() {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/bookRegistrationPage' element={<BookRegistrationPage />}></Route>
       </Routes>
       <ContextApi />
+      <Home />
       <Footer />
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 

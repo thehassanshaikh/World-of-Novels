ce85949a91ff648754a31d1fc2ff764e42bfd9d6
Ankit Agrawal <ankitagrawal678y@gmail.com>
2023-03-28 12:27:10 +0530

    update book registration page

--- src/pages/bookRegistrationPage/BookRegistrationPage.jsx
+++ src/pages/bookRegistrationPage/BookRegistrationPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import backgroundImage from './backgroundImage-2.jpg'
+import backgroundImage from './mintGreenBooks.jpg'
 
 function BookRegistrationPage() {
   const options = ['Fiction', 'NonFiction'];
@@ -7,36 +7,36 @@ function BookRegistrationPage() {
 
   return (
     <>
-    <div className="min-h-screen py-40 bg-gradient-to-r from-stone-700 via-gray-800 to-zinc-400" >
+    <div className="min-h-screen py-40 bg-gradient-to-r from-[#9ccddf] via-[#6a9bad] to-[#51899e]" >
     <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
+        <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-[#d1fbed] rounded-2xl mx-auto shadow-lg overflow-hidden">
           <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center" style={{
             backgroundImage:
               `url(${backgroundImage})`,
           }}>
-            <h1 className="text-white text-3xl mb-3">Welcome</h1>
+            <h1 className="text-[#ffc000] text-3xl mb-3">Welcome</h1>
             <div>
-              <p className="text-white">"Remember: A best-selling book usually follows a simple rule, It's a wonderful story, wonderfully told; not, It's a wonderfully told story."<span className='text-[#1e3042] font-bold'>~ Nicholas Sparks</span></p>
+              <p className="text-[#ffc000] text-xl">"Remember: A best-selling book usually follows a simple rule, It's a wonderful story, wonderfully told; not, It's a wonderfully told story."<span className='text-[#00372e] font-semibold'>~ Nicholas Sparks</span></p>
             </div>
           </div>
           <div className="w-full lg:w-1/2 py-16 px-12">
-            <h2  className="text-3xl mb-4 text-[#34495E]">Register Your Book</h2>
-            <p className="mb-4 text-[#34495E]">
+            <h2  className="text-3xl mb-4 text-[#00372e]">Register Your Book</h2>
+            <p className="mb-4 text-[#00372e]">
               Register your Novel. It’s free and only take a minute
             </p>
             <form action="#">
               
               <div className="mt-5">
-                <input type="text" placeholder="Title" className="border border-gray-400 py-1 px-2 w-full" />
+                <input type="text" placeholder="Title" className="border border-[#00372e] py-1 px-2 w-full" />
               </div>
               <div className="mt-5">
-                <input type="password" placeholder="Author Name" className="border border-gray-400 py-1 px-2 w-full" />
+                <input type="password" placeholder="Author Name" className="border border-[#00372e] py-1 px-2 w-full" />
               </div>
               <div className="grid grid-cols-2 gap-5 mt-5">
-                <input type="text" placeholder="MRP in Rs" className="border border-gray-400 py-1 px-2" />
+                <input type="text" placeholder="MRP in Rs" className="border border-[#00372e] py-1 px-2" />
               </div>
               <div className="grid grid-cols-2 gap-5 mt-5">
-              <select className="block w-full py-2 mb-4 text-sm text-gray-600 border border-gray-400">
+              <select className="block w-full py-2 mb-4 text-sm text-[#00372e] border border-[#00372e]">
               <option>Select Book Category</option>
                     {options.map((option, index) => {
                         return <option key={index} >
@@ -44,7 +44,7 @@ function BookRegistrationPage() {
                         </option>
                     })}
               </select>
-              <select className="block w-full py-2 mb-4 text-sm text-gray-600 border border-gray-400">
+              <select className="block w-full py-2 mb-4 text-sm text-[#00372e] border border-[#00372e]">
                 <option>Select Book Condition</option>
                 {
                   optionsCondition.map((option,index) => {
@@ -56,18 +56,18 @@ function BookRegistrationPage() {
               </select>
               </div>
               <div className='mt-5'> 
-              <label className="block mb-2 text-sm font-medium text-gray-500 dark:text-white" htmlFor="file_input">Upload Picture</label>
-              <input className="block w-full text-sm text-gray-900 border border-gray-300 cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 py-1 px-2" id="file_input" type="file" />
+              <label className="block mb-2 text-sm font-medium text-[#00372e] dark:text-white" htmlFor="file_input">Upload Picture</label>
+              <input className="block w-full text-sm text-[#00372e] border border-[#00372e] cursor-pointer bg-gray-50 focus:outline-none py-1 px-2" id="file_input" type="file" />
               </div>
 
               <div className="mt-5">
-                <input type="checkbox" className="border border-gray-400" />
-                <span>
-                  I accept the <a href="#" className="text-[#34495E] font-semibold">Terms of Use</a> &  <a href="#" className="text-[#34495E] font-semibold">Privacy Policy</a> 
+                <input type="checkbox" className="border border-[#00372e]" />
+                <span className='text-[#00372e]'>
+                  I accept the <a href="#" className="text-[#ffc000] font-semibold">Terms of Use</a> &  <a href="#" className="text-[#ffc000] font-semibold">Privacy Policy</a> 
                 </span>
               </div>
               <div className="mt-5">
-                <button className="w-full bg-[#34495E] py-3 text-center text-white">Register Now</button>
+                <button className="w-full bg-[#00372e] py-3 text-center text-white">Register Now</button>
               </div>
             </form>
           </div>

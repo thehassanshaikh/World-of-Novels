4320171cc3d94b868579ebdb62711627a8789828
thehassanshaikh <thehasanshaikh@gmail.com>
2023-03-29 11:13:53 +0530

    created context component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { ContextApi } from './Components/Context/ContextApi';
+import { BooksList } from './Context/Context';
 import Login from './pages/login/Login';
 import BookRegistrationPage from './pages/bookRegistrationPage/BookRegistrationPage';
 import StartingSearchPage from './pages/startingSearchPage/StartingSearchPage'
@@ -11,13 +11,14 @@ import { Home } from './pages/Home/Home';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/bookRegistrationPage' element={<BookRegistrationPage />}></Route>
-      </Routes>
-      <ContextApi />
-      <Home />
-      <Footer />
+      <BooksList>
+        <Routes>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/bookRegistrationPage' element={<BookRegistrationPage />}></Route>
+        </Routes>
+        <Home />
+        <Footer />
+      </BooksList>
     </BrowserRouter>
   );
 }

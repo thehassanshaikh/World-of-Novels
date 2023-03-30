8245c07d53f7100e0f252bd21b476cfcde965c95
thehassanshaikh <thehasanshaikh@gmail.com>
2023-03-30 11:17:54 +0530

    added some data on data.js file

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const fiction = [
     },
     {
         id: 4,
-        subcategory : 'Fairy tales, fables, and folk tales',
+        subcategory : 'Fairy tales',
     },
     {
         id: 5,
@@ -101,3 +101,90 @@ export const nonFiction=[
     },
 ]
 
+
+export const NovalsData = [{
+    title: "Touching the Void",
+    author: "Joe Simpson",
+    price: 1112,
+    category: "Fiction",
+    condition: "0",
+    description: "A thrilling and emotional true story of survival in the treacherous Peruvian Andes. When climbing partners Joe and Simon reach the summit of Siula Grande, they have no idea what horrors await them on the descent. After an unthinkable tragedy, Simon is forced to make a gut-wrenching decision that tests their friendship to the limit. Touching the Void is a gripping and heart-wrenching tale of human endurance and the power of friendship.",
+    image: "https://img.readthistwice.com/unsafe/240x360/books/fd507387-2bf3-4f28-afe6-8dc73e647fac.jpg",
+    subcategory: ["Thrillers","War","Adventure stories"],
+    coin: 111,
+  },
+  {
+    title: "The Long Walk",
+    author: "Slavomir Rawicz",
+    price: 1422,
+    category: "Fiction",
+    condition: "1",
+    description: "Escape from a Soviet labor camp sets the stage for an epic journey through Siberia, China, the Gobi Desert, Tibet, and the Himalayas in this harrowing tale of survival and freedom. Written with haunting detail and heart, The Long Walk is a testament to human resilience and the fight for liberty. With a new afterword by the author and inspiring foreword to the Polish edition, this updated paperback is sure to leave a lasting impression on readers. Don't miss the upcoming film adaptation directed by Peter Weir in 2011.",
+    shortDescription: "The True Story Of A Trek To Freedom",
+    image: "https://img.readthistwice.com/unsafe/240x360/books/1eccc2bd-7502-4f1e-8c10-aaa5d05af528.jpg",
+    subcategory: ["Historical fiction","Adventure stories"],
+    coin: 94,
+  },
+  {
+    title: "Jane Eyre",
+    author: "Charlotte Brontë",
+    price: 185,
+    category: "Fiction",
+    condition: "2",
+    description: "This classic tale tells the story of an orphaned young woman named Jane who struggles with feeling like an outcast. When she is hired to care for a young ward at Thornfield Hall, she falls in love with the brooding and mysterious owner, Edward Rochester. However, there is a terrifying secret lurking within the halls of Thornfield, and Jane must face difficult decisions about her future. Will she be left heartbroken and exiled once again?",
+    shortDescription: "",
+    image: "https://img.readthistwice.com/unsafe/240x360/books/cfa08a1f-4931-4ca3-a38c-2cffbb1c915c.jpg",
+    subcategory: ["Classics","Romance"],
+    coin: 61,
+  },
+  {
+    title: "Little Women",
+    author: "Louisa May Alcott",
+    price: 1021,
+    category: "Fiction",
+    condition: "0",
+    description: "Four unique sisters rely on each other during their father's absence in this heartwarming story. From putting on plays to forming secret societies, they face life's ups and downs with the one question always on their minds: will Father make it home safely from war?",
+    image: "https://img.readthistwice.com/unsafe/240x360/books/50f4a1a2-7c9c-4a3e-af00-80d73bd39ad5.jpg",
+    subcategory: ["Classics","Women’s fiction"],
+    coin: 102,
+  },
+  {
+    title: "Helter Skelter",
+    author: "Vincent Bugliosi",
+    price: 863,
+    category: "Fiction",
+    condition: "1",
+    description: "Explore the shocking and disturbing case of Charles Manson and his followers in a book written by the prosecuting attorney of the trial. Discover what motivated Manson's cold-blooded selection of victims and his terrifying hold over his followers, particularly the young women who carried out his orders. This harrowing true crime story is accompanied by 50 pages of black and white photographs.",
+    image: "https://img.readthistwice.com/unsafe/240x360/books/50f4a1a2-7c9c-4a3e-af00-80d73bd39ad5.jpg",
+    subcategory: ["Crime","Historical fiction","Mystery","Thrillers"],
+    coin: 57,
+  },
+  {
+    title: "Say Nothing",
+    author: "Patrick Radden Keefe",
+    price: 395,
+    category: "Fiction",
+    condition: "2",
+    description: "Discover the devastating consequences of a notorious killing in Northern Ireland during The Troubles. Follow award-winning writer Patrick Radden Keefe's intricate narrative, starting with the abduction of Jean McConville, a mother of ten, and delve into the violent guerrilla war that plagued Northern Ireland. From IRA terrorists and their brutal violence to dirty schemes of the British Army, Say Nothing is a tale of passion, betrayal, and anguish that lingers long after the last page.",
+    shortDescription: "A True Story of Murder and Memory in Northern Ireland",
+    image: "https://img.readthistwice.com/unsafe/240x360/books/50f4a1a2-7c9c-4a3e-af00-80d73bd39ad5.jpg",
+    subcategory: ["Crime","Historical fiction","Thrillers"],
+    coin: 30,
+  },
+  {
+    title: "Harry Potter And The Sorcerer's Stone",
+    author: "J. K. Rowling",
+    price: 1844,
+    category: "Fiction",
+    condition: "0",
+    description: "The story of a young boy named Harry, who has endured a miserable life with his cruel relatives. All he knows is poverty, bullying, and the feeling of not belonging. But a letter arrives, inviting him to a magical place filled with friends, sports, and endless wonder. With destiny calling, Harry must navigate a dangerous path if he hopes to survive.",
+    image: "https://img.readthistwice.com/unsafe/240x360/books/0882fdb1-1ea3-42e2-9b38-9d18b66d8cf2.jpg",
+    subcategory: ["Fairy tales","Plays","Adventure stories"],
+    coin:184,
+  },
+]  
+
+
+0 - Good
+1- Average 
+2 - bad

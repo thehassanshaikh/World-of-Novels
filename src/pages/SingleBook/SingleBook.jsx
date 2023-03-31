bde702007e27a0a46979770152c0eef1635b6eea
thehassanshaikh <thehasanshaikh@gmail.com>
2023-03-31 08:14:27 +0530

    Not updated

---
+++ src/pages/SingleBook/SingleBook.jsx
@@ -0,0 +1,10 @@
+import "./SingleBook.css"
+
+export const SingleBook =() =>{
+    retrun (
+        <section className="single-book-section">
+            <h1>Single Book page</h1>
+        </section>
+        
+    )
+}

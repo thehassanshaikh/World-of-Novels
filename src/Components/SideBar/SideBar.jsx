e79b4015f622541cc2220071595ab9fb16a13d9b
thehassanshaikh <thehasanshaikh@gmail.com>
2023-03-31 11:44:44 +0530

    make home page card responsive

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,20 +1,25 @@
 import "./Sidebar.css"
+import { useState,useEffect } from "react";
+import {fiction , nonFiction} from "../../data";
 
 export const SideBar = ()=>{
+
+
     return (
         <section className="sidebar-container">
           <div className="sidebar-section">
+
             <h2 className="Category-lable">Fictions</h2>
             <ul className="category-con">
-                <li><input type="checkbox" className="inpt-box" />Fantacy</li>
-                <li><input type="checkbox" className="inpt-box" />Romach</li>
-                <li><input type="checkbox" className="inpt-box" />Crime</li>
+              {fiction.map((item,index)=>(
+                <li><input type="checkbox" className="inpt-box" />{item.subcategory}</li>
+              ))}
             </ul>
             <h2 className="Category-lable">Non - Fictions</h2>
             <ul className="category-con">
-                <li><input type="checkbox" className="inpt-box" />Fantacy</li>
-                <li><input type="checkbox" className="inpt-box" />Romach</li>
-                <li><input type="checkbox" className="inpt-box" />Crime</li>
+            {nonFiction.map((item,index)=>(
+                <li><input type="checkbox" className="inpt-box" />{item.subcategory}</li>
+              ))}
             </ul>
           </div>
         </section>

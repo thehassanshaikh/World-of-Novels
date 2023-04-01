02a1524ab24c2c06c1bd5d19947f5f561f5712fd
Mohammad Hassan shaikh <thehasanshaikh@gmail.com>
2023-04-01 15:43:52 +0530

    added book condion filter in sidebar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,44 +1,50 @@
 import "./Sidebar.css"
-import { useState,useEffect } from "react";
-import {fiction , nonFiction, NovelsData} from "../../data";
+import { useState, useEffect } from "react";
+import { fiction, nonFiction, bookCondition, NovelsData } from "../../data";
 import { UserAuth } from "../../Context/AuthContext";
 
-export const SideBar = ()=>{
+export const SideBar = () => {
   const [q, setQ] = useState("");
-  const {setNovelData}=UserAuth();
-  const [search,setSearch]=useState([]);
-  const filterNovels = (e) =>{
-   
-    if(e.target.checked){
-      const searchResult=(NovelsData.filter(novel=>(
+  const { setNovelData } = UserAuth();
+  const [search, setSearch] = useState([]);
+  const filterNovels = (e) => {
+
+    if (e.target.checked) {
+      const searchResult = (NovelsData.filter(novel => (
         novel.subcategory.includes(e.target.name))))
       //  search = search.concat(searchResult); 
-      setSearch((prevval)=>prevval.concat(searchResult))
+      setSearch((prevval) => prevval.concat(searchResult))
       console.log(search)
-     setNovelData(search)
+      setNovelData(search)
     }
-   
+
   }
   //useEffect(()=>{console.log("hello")},[filterNovels])
 
-    return (
-        <section className="sidebar-container">
-          <div className="sidebar-section">
+  return (
+    <section className="sidebar-container">
+      <div className="sidebar-section">
 
-            <h2 className="Category-lable">Fictions</h2>
-            <ul className="category-con">
-              {fiction.map((item,index)=>(
-                <li key={index}><input type="checkbox" name={item.subcategory}className="inpt-box" onChange={filterNovels}/>{item.subcategory}</li>
-              ))}
-            </ul>
-            <h2 className="Category-lable">Non - Fictions</h2>
-            <ul className="category-con">
-            {nonFiction.map((item,index)=>(
-                <li key={index}><input type="checkbox" name={item.subcategory} className="inpt-box" />{item.subcategory}</li>
-              ))}
-            </ul>
-          </div>
-        </section>
-        
-    )
+        <h2 className="Category-lable">Fictions</h2>
+        <ul className="category-con">
+          {fiction.map((item, index) => (
+            <li key={index}><input type="checkbox" name={item.subcategory} className="inpt-box" onChange={filterNovels} />{item.subcategory}</li>
+          ))}
+        </ul>
+        <h2 className="Category-lable">Non - Fictions</h2>
+        <ul className="category-con">
+          {nonFiction.map((item, index) => (
+            <li key={index}><input type="checkbox" name={item.subcategory} className="inpt-box" />{item.subcategory}</li>
+          ))}
+        </ul>
+        <h2 className="Category-lable">Book Condition</h2>
+        <ul className="category-con">
+          {bookCondition.map((item, index) => (
+            <li key={index}><input type="checkbox" name={item.condition} className="inpt-box" />{item.condition}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
+
+  )
 }

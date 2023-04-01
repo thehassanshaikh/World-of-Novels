cfc7394ab2bb275228af0eb1833671e382d0cf2e
Zahra6653 <zahrasaktiwala@gmail.com>
2023-04-01 14:46:46 +0530

    Resolved

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,16 +2,12 @@
 import React from "react";
 import Footer from "../Footer/Footer";
 import { Home } from "../../pages/Home/Home";
-// import { NovelsList } from "../NovelsList/NovelsList";
-
 import NavBar from "../NavBar/NavBar";
-
 
 function Layout() {
   return (
-    <div className="overflow-hidden">
+    <div>
       <NavBar />
-      {/* <NovelsList /> */}
       <Home />
       <Footer />
     </div>

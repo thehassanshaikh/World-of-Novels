53fde157e42e113fb7cb71474e929eb1e66788b7
Zahra6653 <zahrasaktiwala@gmail.com>
2023-04-01 14:31:41 +0530

    commited

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,20 +2,17 @@ import React from "react";
 import Footer from "../Footer/Footer";
 import { Home } from "../../pages/Home/Home";
 // import { NovelsList } from "../NovelsList/NovelsList";
-import { BooksList } from "../../Context/Context";
+
 import NavBar from "../NavBar/NavBar";
 
 
 function Layout() {
   return (
-    <div>
-
-      <BooksList />
+    <div className="overflow-hidden">
       <NavBar />
       {/* <NovelsList /> */}
       <Home />
       <Footer />
-      <BooksList />
     </div>
   );
 }
